5c79d4f5b20f82f526aa8b723447b39d42333e16
Sachinsharma0237 <sachinsharma0237@gmail.com>
2023-06-04 21:22:36 +0530

    portfolio complete code

---
+++ src/components/products/Data.jsx
@@ -0,0 +1,30 @@
+import Image1 from "../../assets/humanize.jpg";
+import Image2 from "../../assets/safalta.svg";
+import Image3 from "../../assets/poker.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Humanize App",
+    link: "https://humanize.com/",
+    description:
+      "Humanize is a global digital platform supporting personal development through an innovative science-based curriculum and inter-subjective dyadic practices with high-level teachers, regular coaching, and a community to support ongoing and sustainable practice and inner transformation.",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Safalta App",
+    link: "https://www.safalta.com/",
+    description:
+      "Safalta.com, (Safalta Education Pvt. Ltd.) as an initiative, has always aimed at thinking and planning ahead of time for the young aspirants of India. Safalta intends to innovate in the rising arena of Online Education that caters to diligent and bright minds. Here, we provide students with an overall learning experience, while ensuring that our learners gain the required guidance and knowledge to pave their way to success and a bright future.",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Poker Magnet",
+    link: "https://pokermagnet.com/",
+    description:
+      "Poker game, Players can play both free online poker games or choose to play poker online with real chips.",
+  },
+];
